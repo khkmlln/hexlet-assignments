b09288142d9b26ddc556416b2e45fe9756270b6a
@hexlet/cli <support@hexlet.io>
2023-11-27 00:25:12 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -26,8 +26,9 @@ class App {
         StringBuilder filtered = new StringBuilder();
         filtered.append("{\n");
         for (var word : wordCount.keySet()) {
-            filtered.append("  ").append(word).append(": ").append(wordCount.get(word)).append(",\n");
+            filtered.append("  ").append(word).append(": ").append(wordCount.get(word)).append("\n");
         }
+	stringBuilder.append("}");
         return filtered.toString();
     }
 }
