98586f868c9c4cae922c37c809a8f71281a68047
@hexlet/cli <support@hexlet.io>
2023-11-28 01:38:16 +0300

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -7,24 +7,24 @@ import java.util.Map.Entry;
 
 // BEGIN
 class App {
-        public static List<Map<String, String>> findWhere(List<Map<String, String> books, Map<String, String> dictionary) {
-                List<Map<String, String>> final = new ArrayList<>();
+        public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> dictionary) {
+                List<Map<String, String>> finale = new ArrayList<>();
                 for (Map <String, String> book : books) {
                         boolean matched = true;
 			for (Map.Entry<String, String> entry : dictionary.entrySet()) {
-                                String key = dictionary.getKey();
-                                String value = dictionary.getValue();
+                                String key = entry.getKey();
+                                String value = entry.getValue();
                                 String bookKey = book.get(key);
-                                if (!value.getEquals(bookKey)) {
-                                        match = false;
+                                if (!value.Equals(bookKey)) {
+                                        matched = false;
                                         break;
                                 }
                         }
-                        if (match) {
-                                final.add(book);
+                        if (matched) {
+                                finale.add(book);
                         }
                  }
-                return final;
+                return finale;
         }
 }
 //END
