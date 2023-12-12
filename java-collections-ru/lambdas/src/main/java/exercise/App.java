34522b4cd90bc8d77237bfea8aab12a0902c25de
@hexlet/cli <support@hexlet.io>
2023-12-13 01:07:58 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -0,0 +1,21 @@
+package exercise;
+
+
+// BEGIN
+class App {
+    public static String[][] enlargeArrayImage(String[][] array) {
+        var dlina = array.length;
+        var shirina = array[0].length;
+        String[][] changedArray = new String[dlina * 2][shirina * 2];
+        for (int i = 0; i < dlina; i++) {
+            for (int j = 0; j < shirina; j++) {
+                changedArray[i * 2][j * 2] = array[i][j];
+                changedArray[i * 2][j * 2 + 1] = array[i][j];
+                changedArray[i * 2 + 1][j * 2] = array[i][j];
+                changedArray[i * 2 + 1][j * 2 + 1] = array[i][j];
+            }
+        }
+        return changedArray;
+    }
+}
+// END
---
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.List;
+import java.time.LocalDate;
+import java.util.stream.Collectors;
+
+// BEGIN
+class Sorter {
+    public static List<Map<String, String>> takeOldestMans(List<Map<String, String>> users) {
+        return users.stream()
+                .filter(user -> user.get("gender").equals("male"))
+                .sorted(Comparator.comparing(user -> LocalDate.parse(user.get("birthday"))))
+                .collect(Collectors.toList());
+    }
+}
+// END
