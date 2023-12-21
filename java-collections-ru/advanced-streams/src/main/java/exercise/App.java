450e4db73cdabd58bafc579bc42497b87db3202c
@hexlet/cli <support@hexlet.io>
2023-12-22 01:39:44 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -15,14 +15,21 @@ class App {
                 .filter(line -> line.trim().startsWith("environment=\""))
                 .map(line -> line.substring(line.indexOf("\"") + 1, line.length() - 1))
                 .collect(Collectors.toList());
+
         Set<String> forwardedVars = env.stream()
                 .flatMap(line -> Arrays.stream(line.split(",")))
                 .filter(variable -> variable.startsWith("X_FORWARDED_"))
                 .map(variable -> {
                     String[] pair = variable.split("=");
-                    return pair[0].substring(13) + "=" + pair[1]; // измените эту часть
+                    if (pair[0].substring(13).startsWith("X_")) {
+                        return pair[0].substring(13) + "=" + pair[1];
+                    } else {
+                        return null;
+                    }
                 })
+                .filter(variable -> variable != null)
                 .collect(Collectors.toSet());
+
         return String.join(",", forwardedVars);
     }
 }
