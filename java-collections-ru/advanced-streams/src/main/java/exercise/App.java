7338ccd1a8985e42352bdb38f882e69d4d6932f3
@hexlet/cli <support@hexlet.io>
2023-12-25 22:59:46 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,7 +1,6 @@
 package exercise;
 
-import java.util.List;
-import java.util.Set;
+
 import java.util.stream.Collectors;
 import java.util.Arrays;
 
@@ -11,25 +10,15 @@ class App {
         if (config.isEmpty()) {
             return "";
         }
-        List<String> env = Arrays.stream(config.split("\n"))
-                .filter(line -> line.trim().startsWith("environment=\""))
-                .map(line -> line.substring(line.indexOf("\"") + 1, line.length() - 1))
-                .collect(Collectors.toList());
-
-        Set<String> forwardedVars = env.stream()
-                .flatMap(line -> Arrays.stream(line.split(",")))
-                .filter(variable -> variable.startsWith("X_FORWARDED_"))
-                .map(variable -> {
-                    String[] pair = variable.split("=");
-                    if (pair[0].substring(13).startsWith("X_")) {
-                        return pair[0].substring(13) + "=" + pair[1];
-                    } else {
-                        return null;
-                    }
-                })
-                .filter(variable -> variable != null)
-                .collect(Collectors.toSet());
-
-        return String.join(",", forwardedVars);
+        String[] lines = config.split("\n"); // делаем сплит чтобы каждая строка была элементом массива
+        return Arrays.stream(lines)
+                .filter(line -> line.startsWith("environment=\"")) // фильтруем все строки начинающиея с "environment="
+                .map(line -> line.replaceAll("environment=\"\"", "")) // затем удаляем из строки ненужные нам "environment=""
+                .map(line -> line.replaceAll("\"\"", "")) // не забывем удалить лишне ковычки
+                .map(line -> line.split(",")) // то что у нас осталось нужно опять разделить через split по запятой
+                .flatMap(Arrays::stream) // т.к резульат split это массив, надо его превратить в стрим, делаем как в начале Arrays.stream
+                .filter(kv -> kv.startsWith("X_FORWARDED_") ) // фильтруем те элемены которые начинаются с "X_FORWARDED_"
+                .map(kv -> kv.replaceAll("X_FORWARDED_", "\"\"")) // затем удаляем из строки ненужные нам "X_FORWARDED_ для удаления используем replaceAll("ЧТО УДАЛЯЕМ", "")// пустая строка так мы типа заменяем строку на ничто тем самым удаляем
+    .collect(Collectors.joining(",")); // собираем все в List строку с элементами разделенными через запяту/
     }
 }
